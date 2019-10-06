00c0702f7d48604231e51e05c191098f5bd0bb6a
Craig Wagner <craig.wagner@gmail.com>
2019-10-06 11:53:57 -0700

    'Initial'

---
+++ FindWallpaper/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Linq;
+
+namespace FindWallpaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var registryKey = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop");
+            var value = registryKey.GetValue("TranscodedImageCache");
+
+            if (value is byte[] cache)
+            {
+                var bytes = cache.Skip(24).Where(c => c > 0).ToArray();
+                var path = System.Text.Encoding.ASCII.GetString(bytes);
+
+                Process.Start("explorer.exe", $"/select,{path}");
+            }
+        }
+    }
+}
